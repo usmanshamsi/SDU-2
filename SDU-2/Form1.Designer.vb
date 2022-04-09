9a6cf4c37d3324c8a1d92ea99fb8b7cd0f0060c6
usman <usman.shamsi@live.com>
2022-04-09 23:04:03 +0500

    Initial commit

---
+++ SDU-2/Form1.Designer.vb
@@ -0,0 +1,323 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.beam_width = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.area_steel = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.steel_fy = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.conc_fc = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.cover = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.beam_depth = New System.Windows.Forms.TextBox()
+        Me.rtf = New System.Windows.Forms.RichTextBox()
+        Me.calculate_button = New System.Windows.Forms.Button()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(510, 448)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(122, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Width of Beam (b)"
+        '
+        'beam_width
+        '
+        Me.beam_width.Location = New System.Drawing.Point(771, 445)
+        Me.beam_width.Name = "beam_width"
+        Me.beam_width.Size = New System.Drawing.Size(85, 22)
+        Me.beam_width.TabIndex = 0
+        Me.beam_width.Text = "12"
+        Me.beam_width.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(862, 448)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "inch"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(510, 588)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(180, 17)
+        Me.Label11.TabIndex = 15
+        Me.Label11.Text = "Area of Reinforcement (As)"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(862, 588)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(57, 17)
+        Me.Label12.TabIndex = 17
+        Me.Label12.Text = "sq. inch"
+        '
+        'area_steel
+        '
+        Me.area_steel.Location = New System.Drawing.Point(771, 585)
+        Me.area_steel.Name = "area_steel"
+        Me.area_steel.Size = New System.Drawing.Size(85, 22)
+        Me.area_steel.TabIndex = 5
+        Me.area_steel.Text = "1.2"
+        Me.area_steel.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(510, 560)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(196, 17)
+        Me.Label9.TabIndex = 12
+        Me.Label9.Text = "Tensile Strength of Rebar (fy)"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(862, 560)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(26, 17)
+        Me.Label10.TabIndex = 14
+        Me.Label10.Text = "psi"
+        '
+        'steel_fy
+        '
+        Me.steel_fy.Location = New System.Drawing.Point(771, 557)
+        Me.steel_fy.Name = "steel_fy"
+        Me.steel_fy.Size = New System.Drawing.Size(85, 22)
+        Me.steel_fy.TabIndex = 4
+        Me.steel_fy.Text = "60000"
+        Me.steel_fy.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(510, 532)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(252, 17)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "Compressive Strength of Concrete (f'c)"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(862, 532)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(26, 17)
+        Me.Label8.TabIndex = 11
+        Me.Label8.Text = "psi"
+        '
+        'conc_fc
+        '
+        Me.conc_fc.Location = New System.Drawing.Point(771, 529)
+        Me.conc_fc.Name = "conc_fc"
+        Me.conc_fc.Size = New System.Drawing.Size(85, 22)
+        Me.conc_fc.TabIndex = 3
+        Me.conc_fc.Text = "3000"
+        Me.conc_fc.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(510, 504)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(166, 17)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Cover to Center of Rebar"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(862, 504)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(34, 17)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "inch"
+        '
+        'cover
+        '
+        Me.cover.Location = New System.Drawing.Point(771, 501)
+        Me.cover.Name = "cover"
+        Me.cover.Size = New System.Drawing.Size(85, 22)
+        Me.cover.TabIndex = 2
+        Me.cover.Text = "2.5"
+        Me.cover.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(510, 476)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(173, 17)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Overall Depth of Beam (h)"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(862, 476)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(34, 17)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "inch"
+        '
+        'beam_depth
+        '
+        Me.beam_depth.Location = New System.Drawing.Point(771, 473)
+        Me.beam_depth.Name = "beam_depth"
+        Me.beam_depth.Size = New System.Drawing.Size(85, 22)
+        Me.beam_depth.TabIndex = 1
+        Me.beam_depth.Text = "24"
+        Me.beam_depth.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'rtf
+        '
+        Me.rtf.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.rtf.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.rtf.Font = New System.Drawing.Font("Consolas", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rtf.Location = New System.Drawing.Point(1004, 10)
+        Me.rtf.Name = "rtf"
+        Me.rtf.Size = New System.Drawing.Size(292, 709)
+        Me.rtf.TabIndex = 0
+        Me.rtf.TabStop = False
+        Me.rtf.Text = ""
+        '
+        'calculate_button
+        '
+        Me.calculate_button.Location = New System.Drawing.Point(719, 622)
+        Me.calculate_button.Name = "calculate_button"
+        Me.calculate_button.Size = New System.Drawing.Size(177, 34)
+        Me.calculate_button.TabIndex = 6
+        Me.calculate_button.Text = "Design for Flexure"
+        Me.calculate_button.UseVisualStyleBackColor = True
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(387, 200)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(39, 17)
+        Me.Label13.TabIndex = 2
+        Me.Label13.Text = "kip-ft"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(296, 197)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(85, 22)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "50"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(35, 200)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(162, 17)
+        Me.Label14.TabIndex = 0
+        Me.Label14.Text = "Design Bending Moment"
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.calculate_button
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1308, 731)
+        Me.Controls.Add(Me.rtf)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.calculate_button)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.area_steel)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.beam_width)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.steel_fy)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.beam_depth)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.conc_fc)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.cover)
+        Me.Controls.Add(Me.Label6)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "SDU-1 (Rectangular Beam Analysis)"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents beam_width As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents area_steel As System.Windows.Forms.TextBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents steel_fy As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents conc_fc As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents cover As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents beam_depth As System.Windows.Forms.TextBox
+    Friend WithEvents rtf As System.Windows.Forms.RichTextBox
+    Friend WithEvents calculate_button As System.Windows.Forms.Button
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+
+End Class
---
+++ SDU-2/Form1.vb
@@ -0,0 +1,196 @@
+﻿Imports SDU_1.FormSerialisation
+
+Public Class Form1
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click, Label14.Click
+
+    End Sub
+
+    Private Sub calculate_button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles calculate_button.Click
+        Dim b, h, cc, d, fc, fy, _as As Double
+        Try
+            b = Convert.ToDouble(beam_width.Text)
+            h = Convert.ToDouble(beam_depth.Text)
+            cc = Convert.ToDouble(cover.Text)
+            d = h - cc
+            fc = Convert.ToDouble(conc_fc.Text)
+            fy = Convert.ToDouble(steel_fy.Text)
+            _as = Convert.ToDouble(area_steel.Text)
+        Catch ex As Exception
+            MsgBox(ex.Message, vbOKOnly, "Error in inputs")
+        End Try
+
+
+        'print inputs
+        rtf.Clear()
+
+        rtf.AppendText("I N P U T S" & vbCrLf)
+        rtf.AppendText("-----------" & vbCrLf)
+        _blue() : Output("Width of beam, b", b, "inch")
+        _blue() : Output("Depth of beam, h", h, "inch")
+        _blue() : Output("Cover to rebar centre", cc, "inch")
+        _blue() : Output("Specified Strength of Concrete, f'c", fc, "psi")
+        _blue() : Output("Yield Strength of reinforcement, fy", fy, "psi")
+        _blue() : Output("Area of reinforcement, As", _as, "sq.inch")
+        br()
+
+        'print calculations and results
+
+        rtf.AppendText("O U T P U T S" & vbCrLf)
+        rtf.AppendText("-------------" & vbCrLf)
+
+        Output("Effective depth of beam, d", d, "inch")
+        br()
+
+        Dim beta1 As Double = CalculateBeta1(fc)
+        Output("Beta-1", beta1, "0.0###", "")
+        br()
+
+        Dim rho As Double = _as / (b * d)
+
+        Output("Provided reinforcement percentage, rho", rho * 100.0, "%", _as, "sq. inch")
+
+
+
+        Dim rho_min As Double = Math.Max(3 * Math.Sqrt(fc) / fy, 200.0 / fy)
+
+        Output("Minimum reinforcement, rho-minimum", rho_min * 100.0, "%", rho_min * b * d, "sq.inch")
+
+        Dim rho_bal As Double = 0.85 * beta1 * (fc / fy) * (87000 / (87000 + fy))
+        Output("Balanced reinforcement, rho-balance", rho_bal * 100.0, "%", rho_bal * b * d, "sq.inch")
+
+        Const modulus_of_Steel As Double = 29000000.0
+
+        Dim rho_max As Double = ((0.003 + fy / modulus_of_Steel) / 0.008) * rho_bal
+        Output("Maximum reinforcement, rho-max", rho_max * 100.0, "%", rho_max * b * d, "sq.inch")
+        br()
+
+        '    CHECK RHO AGAINST RHO MAX AND RHO MIN
+        If (rho > rho_max) Then
+            If (rho <= rho_bal) Then
+                _red() : Info("rho is greater than rho_max, consider reducing.")
+                br()
+            Else
+                _red() : Info("rho is greater than rho_balance, reduce reinforcement percentage.")
+                Exit Sub
+            End If
+        End If
+
+
+        If (rho < rho_min) Then
+            _red() : Info("rho is less than rho_min, consider increasing.")
+            br()
+        End If
+
+        '    calculate compression block
+
+        Dim a As Double = (_as * fy) / (0.85 * fc * b)
+        Output("Depth of Whitney block, a", a, "inch")
+
+        Dim c As Double = a / beta1
+        Output("Depth of neutral axis, c", c, "inch")
+        br()
+
+        '    calculate strain, phi and moment capacity
+        Dim epsilon_t As Double = ((d - c) / c) * 0.003
+        Output("Net Tensile Strain, epsilon_t", epsilon_t, "0.0#####", "")
+
+        Dim phi As Double = Phi_flexure(epsilon_t)
+        Output("Strength reduction factor, phi_flexure", phi, "0.0###", "")
+        br()
+
+        Dim moment_capacity As Double = _as * fy * (d - a / 2)
+        _green() : Output("Nominal Moment Capacity, Mn", moment_capacity / 12000.0, "kip-ft")
+        _green() : Output("Design Moment Capacity, Mu = phi * Mn", phi * moment_capacity / 12000.0, "kip-ft")
+
+
+    End Sub
+
+    Private Sub Output(ByVal parameter As String, ByVal value As Double, ByVal unit As String)
+        rtf.AppendText(parameter & " = " & value.ToString("0.0#") & " " & unit & vbCrLf)
+    End Sub
+    Private Sub Output(ByVal parameter As String, ByVal value As Double, ByVal format As String, ByVal unit As String)
+        rtf.AppendText(parameter & " = " & value.ToString(format) & " " & unit & vbCrLf)
+    End Sub
+    Private Sub Output(ByVal parameter As String, ByVal value1 As Double, ByVal unit1 As String, ByVal value2 As Double, ByVal unit2 As String)
+        rtf.AppendText(parameter & " = " & value1.ToString("0.0#") & " " & unit1 & " = " & value2.ToString("0.0#") & " " & unit2 & vbCrLf)
+    End Sub
+    Private Sub Output(ByVal parameter As String, ByVal value1 As Double, ByVal format1 As String, ByVal unit1 As String, ByVal value2 As Double, ByVal format2 As String, ByVal unit2 As String)
+        rtf.AppendText(parameter & " = " & value1.ToString(format1) & " " & unit1 & " = " & value2.ToString(format2) & " " & unit2 & vbCrLf)
+    End Sub
+    Private Sub Info(ByVal text As String)
+        rtf.AppendText(text & vbCrLf)
+    End Sub
+    Private Sub br()
+        rtf.AppendText(vbCrLf)
+    End Sub
+    Private Sub _blue()
+        rtf.SelectionColor = Color.Blue
+    End Sub
+    Private Sub _red()
+        rtf.SelectionBackColor = Color.Red
+        'rtf.SelectionColor = Color.DarkRed
+    End Sub
+    Private Sub _green()
+        rtf.SelectionBackColor = Color.Yellow
+        'rtf.SelectionColor = Color.DarkGreen
+
+    End Sub
+    Private Sub _black()
+        rtf.SelectionColor = Color.Black
+    End Sub
+
+    Function CalculateBeta1(ByVal fc As Double) As Double
+
+        ' fc in psi
+
+        If (fc <= 4000) Then
+
+            Return 0.85
+
+        ElseIf (fc >= 8000) Then
+
+            Return 0.65
+
+        Else
+
+            Return 0.85 - (fc - 4000) / 1000 * 0.05
+
+        End If
+    End Function
+
+    Function Phi_flexure(ByVal epsilon_t As Double) As Double
+
+        ' epsilon_t is the strain in tension fibre
+
+        If (epsilon_t >= 0.005) Then
+
+            Return 0.9
+
+        Else
+            Return 0.65 + (epsilon_t - 0.002) * (250.0 / 3.0)
+
+        End If
+    End Function
+
+
+
+
+
+    Private Sub beam_width_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles beam_width.Enter, beam_width.Click, _
+        beam_depth.Enter, beam_depth.Click, cover.Enter, cover.Click, conc_fc.Enter, conc_fc.Click, steel_fy.Enter, steel_fy.Click, _
+        area_steel.Enter, area_steel.Click, TextBox1.Enter, TextBox1.Click
+
+        sender.SelectAll()
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        FormSerialisor.Deserialise(Me, Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) & "\sdu1-data.xml")
+    End Sub
+
+
+    Private Sub Form1_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        FormSerialisor.Serialise(Me, Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) & "\sdu1-data.xml")
+    End Sub
+End Class
